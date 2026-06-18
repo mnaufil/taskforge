c74a5ccdba4a200483715494421570332e58710e
Naufil <mnaufilshaikh@gmail.com>
2026-06-18 14:50:19 +0530

    feat: add register endpoint skeleton

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\AuthService;
+use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct(private readonly AuthService $authService)
+    {
+        
+    }
+
+    public function register(RegisterRequest $request):JsonResponse
+    {
+        return response()->json([
+            'message' => 'Register Endpoints Works'
+        ]);
+    }
+
+
+
+
 }
---
+++ app/Services/AuthService.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+class AuthService
+{
+    public function register(array $data)
+    {
+    }
+
+    public function login(array $data)
+    {
+    }
+
+    public function logout(User $user)
+    {
+    }
+}
